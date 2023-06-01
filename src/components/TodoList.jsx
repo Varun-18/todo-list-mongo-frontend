3f44372d0e5318f576c15330fcb380f0b30f5328
Varun-18 <109971426+Varun-18@users.noreply.github.com>
2023-06-01 17:36:17 +0530

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+import { Button ,Stack} from '@mui/material'
 import React from 'react'
 import useLogout from '../talons/useLogout'
 import { AddTodo } from './AddTodo'
@@ -8,13 +8,18 @@ export const TodoList = () => {
   const { userLogout} = useLogout()
   return (
     <div>
-      <div>
-
-      <h1>Todo list App</h1>
-        <Button onClick={()=>userLogout()}>
-          logout
-      </Button>
-      </div>
+    <Stack
+                display="flex"
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{ padding: '0 10px' }}
+            >
+                <h1>Todo list App</h1>
+                <Button variant="contained" onClick={() => userLogout()}>
+                    logout
+                </Button>
+            </Stack>
       <AddTodo />
     </div>
   )
